7488bd6896a77a84fa2b203c36825c58f9732e20
Flávio Félix <fau_felix30@hotmail.com>
2025-06-05 18:55:30 -0300

    feat: Starting the tic tac toe logic 2

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ class Jogador {
   }
 }
 
+class Jogada {
+  constructor(linha, coluna) {
+    this.linha = linha;
+    this.coluna = coluna;
+  }
+  get valida() {
+    return this.linha > 0 && this.coluna > 0;
+  }
+  get invalida() {
+    return !this.valida;
+  }
+}
+
 class JogoDaVelha {
   constructor(jogador1 = new Jogador("X"), jogador2 = new Jogador("O")) {
     this.jogador1 = jogador1;
@@ -11,11 +24,60 @@ class JogoDaVelha {
     this.jogadorAtual = this.jogador1;
     this.tamanho = 3;
     this.tabuleiro = this.#iniciarTabuleiro();
+    this.vencedor = null;
   }
   #iniciarTabuleiro() {
     return Array(this.tamanho)
       .fill(0)
       .map(() => Array(this.tamanho).fill(null));
+  }
+
+  jogar(jogada) {
+    if (this.#jogadaValida(jogada)) {
+      this.#adicionar(jogada);
+      this.#trocarJogador();
+    } else {
+      console.log("Jogada inválida");
+    }
+  }
+
+  #jogadaValida(jogada) {
+    if (jogada.invalida) {
+      return false;
+    }
+
+    let { linha, coluna } = jogada;
+    if (linha > this.tamanho || coluna > this.tamanho) {
+      return false;
+    }
+    if (this.#ocupado(jogada)) {
+      return false;
+    }
+    if (this.vencedor) {
+      return false;
+    }
+    return true;
+  }
+
+  #ocupado(jogada) {
+    let { linha, coluna } = jogada;
+    return this.#campo(linha, coluna) !== null;
+  }
+
+  #campo(linha, coluna) {
+    return this.tabuleiro[linha - 1][coluna - 1];
+  }
+
+  #trocarJogador() {
+    this.jogadorAtual =
+      this.jogadorAtual.simbolo === this.jogador1.simbolo
+        ? this.jogador2
+        : this.jogador1;
+  }
+
+  #adicionar(jogada) {
+    let { linha, coluna } = jogada;
+    this.tabuleiro[linha - 1][coluna - 1] = this.jogadorAtual.simbolo;
   }
 
   toString() {
@@ -28,5 +90,8 @@ class JogoDaVelha {
 }
 
 const jogo = new JogoDaVelha();
-
+jogo.jogar(new Jogada(1, 1)); // X
+jogo.jogar(new Jogada(1, 2)); // O
+jogo.jogar(new Jogada(2, 2)); // X
+jogo.jogar(new Jogada(1, 1)); // O
 console.log(jogo.toString());
